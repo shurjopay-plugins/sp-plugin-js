46cb6e488990d8515e8358d5b758564cfeb2d638
shant0 <shanto@pranisheba.com.bd>
2022-11-29 16:27:36 +0600

    Modify parmas

--- ShurjoPay.js
+++ ShurjoPay.js
@@ -63,40 +63,32 @@ async function authentication() {
  * This method is used for making payment.
  *
  * @param Payment request object. See the shurjoPay version-2 integration documentation(beta).docx for details.
- * @return Payment response object contains redirect URL to reach payment page,token_details,order_id, formdata, client_ip to verify order in shurjoPay.
+ * @return Payment response object contains redirect URL to reach payment page,token_details,order_id, form_data to verify order in shurjoPay.
  * @throws ShurjopayException while merchant username and password is invalid.
  * @throws ShurjopayPaymentException while {#link PaymentReq} is not prepared properly or {#link HttpClient} exception
  */
-async function makePayment(token_details, order_id, formdata, client_ip) {
+async function makePayment(token_details, order_id, form_data) {
+  //Getting Ip from User device
+  const ip = await fetch("https://checkip.amazonaws.com/");
+  const client_ip = await ip.text();
+  const phone_regx = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+  const email_regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const { token, token_type, store_id } = token_details;
   let makePayment_details = " ";
-  if (formdata) {
-    const {
-      amount,
-      currency,
-      customer_phone,
-      customer_name,
-      customer_city,
-      customer_address,
-      customer_post_code,
-      customer_email,
-    } = formdata;
-    const phone_regx = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
-    const email_regx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (form_data) {
     if (
       token_type &&
       token &&
       store_id &&
-      amount > 9 &&
-      currency &&
-      customer_phone.match(phone_regx) &&
-      customer_name.length > 1 &&
-      customer_city.length > 1 &&
-      customer_address.length > 1 &&
-      customer_post_code.length === 4 &&
-      customer_email.match(email_regx) &&
-      order_id &&
-      client_ip
+      form_data.amount > 9 &&
+      form_data.currency &&
+      form_data.customer_phone.match(phone_regx) &&
+      form_data.customer_name.length > 1 &&
+      form_data.customer_city.length > 1 &&
+      form_data.customer_address.length > 1 &&
+      form_data.customer_post_code.length === 4 &&
+      form_data.customer_email.match(email_regx) &&
+      order_id
     ) {
       await fetch(`${url}/api/secret-pay`, {
         method: "POST",
@@ -107,19 +99,19 @@ async function makePayment(token_details, order_id, formdata, client_ip) {
         },
         body: JSON.stringify({
           prefix: prefix,
-          store_id: store_id,
-          token: token,
+          store_id: token_details.store_id,
+          token: token_details.token,
           return_url: return_url,
           cancel_url: cancel_url,
-          amount: amount,
+          amount: form_data.amount,
           order_id: order_id,
-          currency: currency,
-          customer_name: customer_name,
-          customer_address: customer_address,
-          customer_phone: customer_phone,
-          customer_city: customer_city,
-          customer_email: customer_email,
-          customer_post_code: customer_post_code,
+          currency: form_data.currency,
+          customer_name: form_data.customer_name,
+          customer_address: form_data.customer_address,
+          customer_phone: form_data.customer_phone,
+          customer_city: form_data.customer_city,
+          customer_email: form_data.customer_email,
+          customer_post_code: form_data.customer_post_code,
           client_ip: client_ip,
         }),
       })
@@ -163,6 +155,7 @@ async function verifyPayment(token_details, sp_order_id) {
     })
       .then((response) => response.json())
       .then((paymentDetails) => {
+        log(paymentDetails);
         verify_status = paymentDetails;
       })
       .catch((error) => {
@@ -205,12 +198,27 @@ async function paymentStatus(token_details, sp_order_id) {
       .catch((error) => {
         console.error("Error:", error);
       });
+
     return payment_status;
   } else {
     return "Require Data Not Found";
   }
 }
 
+async function log(...msgs) {
+  let dirHandle = await window.showDirectoryPicker();
+  const file = await dirHandle.getFileHandle("../log.txt", {
+    create: true,
+  });
+
+  const blob = new Blob(msgs);
+  const writableStream = await file.createWritable();
+  // write our file
+  console.log(blob);
+  await writableStream.write(blob);
+  // close the file and write the contents to disk.
+  await writableStream.close();
+}
 /*
  * Export functions and return values
  */
