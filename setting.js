a3b596ec3072f6a426a28a8e44c154337d6b40fe
shant0 <shanto@pranisheba.com.bd>
2022-12-21 17:38:17 +0600

    readme edit

--- setting.js
+++ setting.js
@@ -1,11 +1,11 @@
 
 const setting = {
-  url: "",
-  username: "",
-  password: "",
-  prefix: "",
-  return_url: "",
-  cancel_url: "",
+  url: process.env.SHURJOPAY_URL,
+  username: process.env.SHURJOPAY_USER_NAME,
+  password:process.env.SHURJOPAY_PASSWORD,
+  prefix: process.env.SHURJOPAY_PREFIX,
+  return_url:process.env.SHURJOPAY_RETURN_URL,
+  cancel_url: process.env.SHURJOPAY_RETURN_URL,
 };
 
 export { setting };
