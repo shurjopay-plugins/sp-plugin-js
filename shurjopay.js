98c2ce9dce70641ba09beb84f1d5d34e3d009b08
shant00 <sshanto.cse@gmail.com>
2023-05-18 17:52:19 +0600

    refactor plugin

--- shurjopay.js
+++ shurjopay.js
@@ -1,4 +1,3 @@
-/*
 /**
  * 
  * Plug-in service to provide shurjoPay gateway services.
@@ -14,7 +13,7 @@ import { shurjopay_config } from '../../shurjopay_config.js';
 
 //Getting credentials from shurjopay_config.js file
 const sp_endpoint = shurjopay_config.SP_ENDPOINT;
-const sp_username= shurjopay_config.SP_USERNAME;
+const sp_username = shurjopay_config.SP_USERNAME;
 const sp_password = shurjopay_config.SP_PASSWORD;
 const sp_prefix = shurjopay_config.SP_PREFIX;
 const sp_return_url = shurjopay_config.SP_RETURN_URL;
@@ -28,31 +27,27 @@ const sp_return_url = shurjopay_config.SP_RETURN_URL;
  * @throws ShurjopayException while merchant username and password is invalid.
  */
 async function authentication() {
-  try {
-      // Fetch payment URL from authenticate API
-    const response = await fetch(`${sp_endpoint}/api/get_token`, {
-      method: "POST",
-       // Request Header credentials
-      headers: {
-        "Content-Type": "application/json",
-      },
-       // Request body credentials
-      body: JSON.stringify({
-        username: sp_username,
-        password: sp_password,
-      }),
-    });
+  // Fetch payment URL from authenticate API
+  const response = await fetch(`${sp_endpoint}/api/get_token`, {
+    method: "POST",
+    // Request Header credentials
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Request body credentials
+    body: JSON.stringify({
+      username: sp_username,
+      password: sp_password,
+    }),
+  });
 
-    if (response.ok) {
-      const tokenDetails = await response.json();
-      return tokenDetails;
-    } else {
-      throw new Error("Check User Name or Password");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
+  if (response.ok) {
+    const tokenDetails = await response.json();
+    return tokenDetails;
+  } else {
+    return "Check User Name or Password";
   }
+
 }
 
 /**
@@ -64,54 +59,50 @@ async function authentication() {
  * @throws ShurjopayPaymentException while {#link PaymentReq} is not prepared properly or {#link HttpClient} exception
  */
 async function makePayment(order_id, form_data) {
-  try {
-    // Getting IP from user device
-    const ipResponse = await fetch("https://checkip.amazonaws.com/");
-    const client_ip = await ipResponse.text();
+  // Getting IP from user device
+  const ipResponse = await fetch("https://checkip.amazonaws.com/");
+  const client_ip = await ipResponse.text();
 
-    // Call authentication function to get token details with store ID
-    const token_details = await authentication();
+  // Call authentication function to get token details with store ID
+  const token_details = await authentication();
+  const { token, token_type, store_id } = token_details;
 
-    const { token, token_type, store_id } = token_details;
-    const payLoad={
-      // Request body credentials
-      prefix: sp_prefix,
-      store_id: store_id,
-      token: token,
-      return_url: sp_return_url,
-      cancel_url: sp_return_url,
-      order_id: order_id,
-      client_ip: client_ip,
-      ...form_data
-    }
-
-    // Validate amount
-    if (form_data.amount <= 0) {
-      throw new Error("Amount cannot be less then or equal to 0");
-    }
-
-    // Fetch payment URL from makePayment API
-    const response = await fetch(`${sp_endpoint}/api/secret-pay`, {
-      method: "POST",
-      headers: {
-        // Request header credentials
-        authorization: `${token_type} ${token}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payLoad),
-    });
-
-    if (response.ok) {
-      const makePaymentDetails = await response.json();
-      return makePaymentDetails;
-    } else {
-      throw new Error("Failed to Payment");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
+  // Request body credentials
+  const payLoad = {
+    prefix: sp_prefix,
+    store_id: store_id,
+    token: token,
+    return_url: sp_return_url,
+    cancel_url: sp_return_url,
+    order_id: order_id,
+    client_ip: client_ip,
+    ...form_data
   }
+
+  // Validate amount
+  if (form_data.amount <= 0) {
+    return "Amount cannot be less then or equal to 0";
+  }
+
+  // Fetch payment URL from makePayment API
+  const response = await fetch(`${sp_endpoint}/api/secret-pay`, {
+    method: "POST",
+  // Request header credentials
+    headers: {
+      authorization: `${token_type} ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payLoad),
+  });
+
+  if (response.ok) {
+    const makePaymentDetails = await response.json();
+    return makePaymentDetails;
+  } else {
+    return "Failed to Payment";
+  }
+
 }
 /**
  * This method is used for verifying order by order id which could be get by payment response object
@@ -122,36 +113,31 @@ async function makePayment(order_id, form_data) {
  * @throws ShurjopayVerificationException while token_type, token, order id is invalid or payment is not initiated properly or {#link HttpClient} exception
  */
 async function verifyPayment(sp_order_id) {
-  try {
-    if (!sp_order_id) {
-      return "Order Id Invalid";
-    }
+  if (!sp_order_id) {
+    return "Payment Id Invalid";
+  }
 
-    const token_details = await authentication();
-    const { token, token_type } = token_details;
+  const token_details = await authentication();
+  const { token, token_type } = token_details;
 
-    const response = await fetch(`${sp_endpoint}/api/verification`, {
-      method: "POST",
-      headers: {
-        // Request header credentials
-        authorization: `${token_type} ${token}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // Request body credentials
-        order_id: sp_order_id,
-      }),
-    });
+  const response = await fetch(`${sp_endpoint}/api/verification`, {
+    method: "POST",
+    headers: {
+      // Request header credentials
+      authorization: `${token_type} ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // Request body credentials
+      order_id: sp_order_id,
+    }),
+  });
 
-    if (response.ok) {
-      return await response.json();
-    } else {
-      throw new Error("Payment Verification Fail");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return "Payment Verification Failed";
   }
 }
 
@@ -164,41 +150,36 @@ async function verifyPayment(sp_order_id) {
  * @throws ShurjopayVerificationException while order id is invalid or payment is not initiated properly or {#link HttpClient} exception
  */
 async function paymentStatus(sp_order_id) {
-  try {
-    if (!sp_order_id) {
-      return "Payment Id Invalid";
-    }
+  if (!sp_order_id) {
+    return "Payment Id Invalid";
+  }
 
-    const token_details = await authentication();
-    const { token, token_type } = token_details;
+  const token_details = await authentication();
+  const { token, token_type } = token_details;
 
-    const response = await fetch(`${sp_endpoint}/api/verification`, {
-      method: "POST",
-      headers: {
-        // Request header credentials
-        authorization: `${token_type} ${token}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // Request body credentials
-        order_id: sp_order_id,
-      }),
-    });
+  const response = await fetch(`${sp_endpoint}/api/verification`, {
+    method: "POST",
+    headers: {
+      // Request header credentials
+      authorization: `${token_type} ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // Request body credentials
+      order_id: sp_order_id,
+    }),
+  });
 
-    if (response.ok) {
-      return await response.json();
-    } else {
-      throw new Error("Payment Verification Fail");
-    }
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return "Payment Verification Failed";
   }
 }
 
 /*
  * Export functions and return values
  */
-export {  makePayment, paymentStatus, verifyPayment };
+export { makePayment, paymentStatus, verifyPayment };
 
