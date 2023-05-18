b7c4a4f274b2cd3c8293235cc0e411babcb825e8
shant00 <sshanto.cse@gmail.com>
2023-05-18 16:21:20 +0600

    refactor some isuue

--- shurjopay.js
+++ shurjopay.js
@@ -28,32 +28,30 @@ const sp_return_url = shurjopay_config.SP_RETURN_URL;
  * @throws ShurjopayException while merchant username and password is invalid.
  */
 async function authentication() {
-   //initiate authentication details which is hold api response
-  let token_details = " ";
-  if (sp_username && sp_password) {
-    await fetch(`${sp_endpoint}/api/get_token`, {
+  try {
+      // Fetch payment URL from authenticate API
+    const response = await fetch(`${url}/api/get_token`, {
       method: "POST",
+       // Request Header credentials
       headers: {
         "Content-Type": "application/json",
       },
+       // Request body credentials
       body: JSON.stringify({
-        //request body credentials
-        username: sp_username,
-        password: sp_password,
+        username: username,
+        password: password,
       }),
-    })
-    //response received as json
-      .then((response) => response.json())
-      .then((tokenDetails) => {
-        token_details = tokenDetails;
-      })
-      .catch((error) => {
-         //error handler if any problem to fetch api
-        console.error("Error:", error);
-      });
-    return token_details;
-  } else {
-    return "Authentication Error";
+    });
+
+    if (response.ok) {
+      const tokenDetails = await response.json();
+      return tokenDetails;
+    } else {
+      throw new Error("Check User Name or Password");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
   }
 }
 
@@ -66,75 +64,53 @@ async function authentication() {
  * @throws ShurjopayPaymentException while {#link PaymentReq} is not prepared properly or {#link HttpClient} exception
  */
 async function makePayment(order_id, form_data) {
-  //Getting Ip from User device
-  const ip = await fetch("https://checkip.amazonaws.com/");
-  const client_ip = await ip.text();
+  try {
+    // Getting IP from user device
+    const ipResponse = await fetch("https://checkip.amazonaws.com/");
+    const client_ip = await ipResponse.text();
 
-  //Regular Expression For Checking Email and phone Number validation
-  const phone_regex = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
-  const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // Call authentication function to get token details with store ID
+    const token_details = await authentication();
 
-  //call authentication function for getting token details with store id
-  const token_details = await authentication();
-  const { token, token_type, store_id } = token_details;
-  //initiate make payment details which is hold api response
-  let makePayment_details = " ";
+    const { token, token_type, store_id } = token_details;
+    const payLoad={
+      // Request body credentials
+      prefix: prefix,
+      store_id: store_id,
+      token: token,
+      return_url: return_url,
+      cancel_url: cancel_url,
+      order_id: order_id,
+      client_ip: client_ip,
+      ...form_data
+    }
 
-  //Checking all necessary values and formate
-  if (
-    token_type &&
-    token &&
-    store_id &&
-    form_data.amount > 9 &&
-    form_data.currency &&
-    form_data.customer_phone.match(phone_regex) &&
-    form_data.customer_name.length > 1 &&
-    form_data.customer_city.length > 1 &&
-    form_data.customer_address.length > 1 &&
-    form_data.customer_post_code.length === 4 &&
-    form_data.customer_email.match(email_regex) &&
-    order_id
-  ) {
-    //fetching payment url from makePayment api
-    await fetch(`${sp_endpoint}/api/secret-pay`, {
+    // Validate amount
+    if (form_data.amount <= 0) {
+      throw new Error("Amount cannot be less then or equal to 0");
+    }
+
+    // Fetch payment URL from makePayment API
+    const response = await fetch(`${url}/api/secret-pay`, {
       method: "POST",
       headers: {
-        //request header credentials
+        // Request header credentials
         authorization: `${token_type} ${token}`,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        //request body credentials
-        prefix: sp_prefix,
-        store_id: store_id,
-        token: token,
-        return_url: sp_return_url,
-        cancel_url: sp_return_url,
-        amount: form_data.amount,
-        order_id: order_id,
-        currency: form_data.currency,
-        customer_name: form_data.customer_name,
-        customer_address: form_data.customer_address,
-        customer_phone: form_data.customer_phone,
-        customer_city: form_data.customer_city,
-        customer_email: form_data.customer_email,
-        customer_post_code: form_data.customer_post_code,
-        client_ip: client_ip,
-      }),
-    })
-      //response received as json
-      .then((response) => response.json())
-      .then((makePaymentDetails) => {
-        makePayment_details = makePaymentDetails;
-      })
-      .catch((error) => {
-        //error handler if any problem to fetch api
-        console.error("Error:", error);
-      });
-    return makePayment_details;
-  } else {
-    return "shurjoPay Payment Failed";
+      body: JSON.stringify(payLoad),
+    });
+
+    if (response.ok) {
+      const makePaymentDetails = await response.json();
+      return makePaymentDetails;
+    } else {
+      throw new Error("Failed to Payment");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
   }
 }
 /**
@@ -146,37 +122,36 @@ async function makePayment(order_id, form_data) {
  * @throws ShurjopayVerificationException while token_type, token, order id is invalid or payment is not initiated properly or {#link HttpClient} exception
  */
 async function verifyPayment(sp_order_id) {
-  //call authentication function for getting token details.
-  const token_details = await authentication();
-  const { token, token_type } = token_details;
-  let verify_status = " ";
-  if (token && token_type && sp_order_id) {
-    await fetch(`${sp_endpoint}/api/verification`, {
+  try {
+    if (!sp_order_id) {
+      return "Order Id Invalid";
+    }
+
+    const token_details = await authentication();
+    const { token, token_type } = token_details;
+
+    const response = await fetch(`${url}/api/verification`, {
       method: "POST",
       headers: {
-        //request header credentials
+        // Request header credentials
         authorization: `${token_type} ${token}`,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        //request body credentials
+        // Request body credentials
         order_id: sp_order_id,
       }),
-    })
-      //response received as json
-      .then((response) => response.json())
-      .then((paymentDetails) => {
-        verify_status = paymentDetails;
-      })
-      .catch((error) => {
-        //error handler if any problem to fetch api
-        console.error("Error:", error);
-      });
+    });
 
-    return verify_status;
-  } else {
-    return "Payment Verification Fail";
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Payment Verification Fail");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
   }
 }
 
@@ -189,41 +164,41 @@ async function verifyPayment(sp_order_id) {
  * @throws ShurjopayVerificationException while order id is invalid or payment is not initiated properly or {#link HttpClient} exception
  */
 async function paymentStatus(sp_order_id) {
-  //call authentication function for getting token details.
-  const token_details = await authentication();
-  const { token, token_type } = token_details;
-  let payment_status = " ";
-  if (token && token_type && sp_order_id) {
-    await fetch(`${sp_endpoint}/api/payment-status`, {
+  try {
+    if (!sp_order_id) {
+      return "Payment Id Invalid";
+    }
+
+    const token_details = await authentication();
+    const { token, token_type } = token_details;
+
+    const response = await fetch(`${url}/api/verification`, {
       method: "POST",
       headers: {
-        //request header credentials
+        // Request header credentials
         authorization: `${token_type} ${token}`,
         Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        //request body credentials
+        // Request body credentials
         order_id: sp_order_id,
       }),
-    })
-      //response received as json
-      .then((response) => response.json())
-      .then((paymentDetails) => {
-        payment_status = paymentDetails;
-      })
-      .catch((error) => {
-        //error handler if any problem to fetch api
-        console.error("Error:", error);
-      });
-    return payment_status;
-  } else {
-    return "payment Status Not Found";
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Payment Verification Fail");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
   }
 }
 
 /*
  * Export functions and return values
  */
-export { authentication, makePayment, paymentStatus, verifyPayment };
+export {  makePayment, paymentStatus, verifyPayment };
 
